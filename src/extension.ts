5b4efc9275d1e3b0ca6fb6635df4842002e00c1e
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-11-29 15:16:57 -0500

    0.0.6

--- src/extension.ts
+++ src/extension.ts
@@ -24,7 +24,10 @@ class CustomLinkProvider implements vscode.DocumentLinkProvider {
         
         // Combine default attributes (src, href) with custom ones
         //const allAttributes = ['src', 'href', 'imp-h', ...customAttributes];
-        const allAttributes = ['imp-h',, 'be-importing', 'be-written', '📥', '📜', ...customAttributes];
+        const allAttributes = [
+            'imp-h', 'be-importing', 'be-written', '📥', '📜',
+            'em-bower-src', '😶‍🌫️-src',
+            ...customAttributes];
         const text = document.getText();
         const parsedHTML = parse(text);
         const lookup: {[key: string]: string} = {};
